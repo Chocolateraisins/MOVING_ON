d390078d6b5d07a455154056e055c11a1150dabe
victor king <victor.king@essec.edu>
2018-11-15 14:57:01 +0800

    yo

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# orders
+#service_item_orders
+#inventory
+#service_items
+#services
+
+#users < orders <inventory
+#services < service_items < services service_item_orders<
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,2 @@
-# User seed
-
-User.create!(
-    first_name: "Rinou",
-    last_name: "Idder",
-    email: "rinou.idder@netherlands.nl",
-    phone: Faker::PhoneNumber.phone_number,
-    address: Faker::Address.street_address,
-    password: "mynameisrinouidder",
-    admin: true,
-    )
-
-20.times do |user|
-  User.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name ,
-    email: Faker::Internet.email,
-    phone: Faker::PhoneNumber.phone_number,
-    address: Faker::Address.street_address,
-    password: [1,2,3,4,5,6,7,8,9,'a','t','b','d','u','x','o'].sample(6).join,
-    admin: false,
-    )
-end
 
 
