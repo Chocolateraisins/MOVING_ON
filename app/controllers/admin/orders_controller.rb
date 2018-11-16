b8d1bc3f83d57ae0662678e4c636df774ee135a2
victor king <victor.king@essec.edu>
2018-11-16 14:31:44 +0800

    further updates

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,11 @@
 class Admin::OrdersController < ApplicationController
+
   def index
-    @orders = Order.all
+    @orders = Order.where.not(latitude: nil, longitude: nil)
+      @markers = @orders.map do |order|
+        { lat: order.latitude,
+          lng: order.longitude
+        }
+      end
   end
 end
