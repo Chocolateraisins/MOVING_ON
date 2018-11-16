a091b3c3a0e0e7e09b8676343ba8bfde120eb697
victor king <victor.king@essec.edu>
2018-11-16 14:52:22 +0800

    done

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,11 @@
 class Admin::OrdersController < ApplicationController
 
   def index
-    @orders = Order.where.not(latitude: nil, longitude: nil)
-      @markers = @orders.map do |order|
-        { lat: order.latitude,
-          lng: order.longitude
+    @orders = Order.all
+    @valid_orders = Order.where.not(latitude: nil, longitude: nil)
+      @markers = @valid_orders.map do |order|
+        { lat: order.deceased_address.latitude,
+          lng: order.longitude_address.longitude
         }
       end
   end
