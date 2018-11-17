c561f0b082d4c15776b21dddfe4924b91f5ab8fa
victor king <victor.king@essec.edu>
2018-11-17 15:53:40 +0800

    test

---
+++ app/controllers/admin/order_items_controller.rb
@@ -0,0 +1,17 @@
+class Admin::OrderItemsController < ApplicationController
+
+  def update
+
+    @order_item = OrderItem.find(params[:id])
+    @order_item.update(order_items_params)
+    redirect_to admin_orders_path
+  end
+
+  private
+
+  def order_items_params
+    params.require(:order_item).permit(:completed)
+  end
+
+
+end
