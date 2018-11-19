fa420c370df8344b6eea8bb920e790f4aff6e770
victor king <victor.king@essec.edu>
2018-11-19 18:41:32 +0800

    styling

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,6 @@ class Admin::OrdersController < ApplicationController
 
   def index
     @orders = Order.all
-
     @orders.each do |order|
       result = Geocoder.search(order.deceased_address)
       order.latitude = result[0]
@@ -10,11 +9,24 @@ class Admin::OrdersController < ApplicationController
     end
 
     @valid_orders = Order.where.not(latitude: nil, longitude: nil)
-      @markers = @valid_orders.map do |valid_order|
-        { lat: valid_order.latitude,
-          lng: valid_order.longitude
-        }
-      end
+    @markers = @valid_orders.map do |valid_order|
+      { lat: valid_order.latitude,
+        lng: valid_order.longitude
+      }
+    end
+
+    @active = 'orders-tab'
+
+    if params[:service] && params[:status]
+      service = params[:service]
+      completed = params[:status] == "Completed"
+      @active = 'tasks-tab'
+      ### maybe change ServiceItem to OrderItem
+      @order_items = ServiceItem.joins(:service).joins(:order_items).where('services.name = ? AND service_items.category = ? AND order_items.completed = ?', service, "task", completed)
+    else
+      @order_items = OrderItem.all
+    end
+
   end
 
   def destroy
