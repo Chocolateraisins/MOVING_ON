4512fb04621eeeac96f09051469fe240c42019b3
Jonas Dieterle <jonas.dieterle@gmail.com>
2018-11-19 15:49:13 +0800

    order confirmation navigation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :inventories
+  has_many :inventories, dependent: :nullify
   has_many :order_items, dependent: :nullify
 
   validates :deceased_first_name, :deceased_last_name, :deceased_address, :date_of_death, presence: true
