ddc7a9c15d97b2d23b5f5aded40a2b42df94e923
Stephen Schuez <stephen.schuez@gmail.com>
2018-11-21 17:15:42 +0800

    if statement for checking if we have the super form

--- app/javascript/components/form.js
+++ app/javascript/components/form.js
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-
-// class formWizard {
-//   constructor() {
-//     this.activeStep = 1;
-//     this.attachListeners();
-//     this.showPrevNextSubmit();
-//   }
-
-//   attachListeners() {
-//     const btn = document.getElementById("next")
-//     if (btn) {
-//       document.getElementById("next").addEventListener("click", this.nextTab.bind(this));
-//       document.getElementById("previous").addEventListener("click", this.previousTab.bind(this));
-//     }
-//   }
-
-//   hideAllSteps() {
-//     var steps = document.querySelectorAll("div.form-step");
-//     steps.forEach((step) => {
-//       step.hidden = true;
-//     });
-//   }
-
-//   nextTab() {
-//     this.activeStep += 1;
-//     this.hideAllSteps();
-//     document.getElementById(`step-${this.activeStep}`).hidden = false;
-//     this.showPrevNextSubmit()
-//     // increment the active tab with 1
-//     // hide all the other form steps hideAllSteps()
-//     // show the active one
-//   }
-
-//     previousTab() {
-//     this.activeStep -= 1;
-//     this.hideAllSteps();
-//     document.getElementById(`step-${this.activeStep}`).hidden = false;
-//     this.showPrevNextSubmit()
-//   }
-
-//   showPrevNextSubmit() {
-//     console.log("helllllo")
-//     if (this.activeStep <= 1) {
-//       document.querySelector("#previous").style.display = "none";
-//     } else {
-//       document.querySelector("#previous").style.display = "inline"
-//     }
-//     if (this.activeStep >= 3) {
-//       document.querySelector("#next").style.display = "none";
-//     } else {
-//       document.querySelector("#next").style.display = "inline"
-//     }
-//     if (this.activeStep < 3) {
-//       document.querySelector("#submit").style.display = "none";
-//     } else {
-//       document.querySelector("#submit").style.display = "inline"
-//     }
-//   }
-// }
-
-// new formWizard()
-=======
 class formWizard {
   constructor() {
     this.activeStep = 2;
@@ -160,7 +97,13 @@ class formWizard {
   }
 }
 
-new formWizard()
->>>>>>> e3836c719c9bab5577577e0a421c8e20f17a4e4b
+const editOrder = document.querySelector(".edit_order");
+
+if (editOrder) {
+  new formWizard();
+}
+
+
+
 
 
