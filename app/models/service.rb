1636256d179fcee83a9a1f38fdea1fcf13b97d9f
Jonas Dieterle <jonas.dieterle@gmail.com>
2018-11-23 10:38:30 +0800

    order form with initial service selection & filtered order show page

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,5 @@ class Service < ApplicationRecord
   has_many :service_items, dependent: :nullify
   accepts_nested_attributes_for :service_items
   validates :name, :description, presence: true
+  has_many :desired_services, dependent: :destroy
 end
